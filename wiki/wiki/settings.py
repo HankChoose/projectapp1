ea098600a28cb48082cad1fcccb0ad163c392b97
HankChoose <hankchenv@gmail.com>
2023-07-02 16:33:17 -0600

    Update settings.py

--- wiki/wiki/settings.py
+++ wiki/wiki/settings.py
@@ -147,7 +147,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=(os.path.join('static'))
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = ''
 #STATIC_ROOT = os.path.join(BASE_DIR, '/wiki/encyclopedia/static')
 #STATIC_ROOT = os.path.join(BASE_DIR, '/encyclopedia/static/')
