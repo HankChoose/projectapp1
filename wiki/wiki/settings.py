08ba160a8a63942f431b90f2e533b28316f6f9a4
HankChoose <hankchenv@gmail.com>
2023-07-02 16:41:28 -0600

    Update settings.py

--- wiki/wiki/settings.py
+++ wiki/wiki/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    '/wiki/encyclopedia/static/',
 ]
 STATIC_ROOT = ''
 #STATIC_ROOT = os.path.join(BASE_DIR, '/wiki/encyclopedia/static')
