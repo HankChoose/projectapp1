19bd1c90ff990682f9156e11e85ea57b80900abc
HankChoose <hankchenv@gmail.com>
2023-07-02 12:18:18 -0600

    a2

--- wiki/wiki/settings.py
+++ wiki/wiki/settings.py
@@ -147,6 +147,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+'''
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
+'''
 
 INTERNAL_IPS = [
     '127.0.0.1'  # 示例：本地开发服务器的IP地址
