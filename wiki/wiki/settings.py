14934b5e883fa167f2d07fab5c35870d539d052f
HankChoose <hankchenv@gmail.com>
2023-07-03 14:02:52 -0600

    a

--- wiki/wiki/settings.py
+++ wiki/wiki/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_URL = '/'
+STATIC_URL = '/static/'
 #STATIC_ROOT = '/etc/nginx/html/static/'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 #STATIC_ROOT = os.path.join(ROOT_PATH,'static/')
